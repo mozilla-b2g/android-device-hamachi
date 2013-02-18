acb3dca8f57d09c2590c2e93a3061846d6204cc9
Michael Wu <mwu@mozilla.com>
2013-02-18 11:35:45 -0500

    Update quirks/properties

--- full_hamachi.mk
+++ full_hamachi.mk
@@ -12,9 +12,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
   rild.libpath=/system/lib/libril-qc-1.so \
   rild.libargs=-d/dev/smd0 \
   ro.use_data_netmgrd=true \
-  ro.moz.cam.0.sensor_offset=270 \
   ro.moz.ril.simstate_extra_field=true \
-  ro.moz.ril.emergency_by_default=true
+  ro.moz.ril.emergency_by_default=true \
+  ro.display.colorfill=1 \
+  ro.moz.omx.hw.max_width=640 \
+  ro.moz.omx.hw.max_height=480 \
+
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_hamachi
